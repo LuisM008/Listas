d0203661a82e793de17dc87532c40c26b3158dff
LuisM008 <luiseduardo.monico@upaep.mx>
2026-03-26 08:38:16 -0600

    Create inventario.cs

---
+++ inventario.cs
@@ -0,0 +1,41 @@
+using System;
+
+public class Inventario
+{
+    //private List<Producto>productos=new List<Producto>
+    
+    public void AgregarProducto ()
+    {
+        
+    }
+    public void ListaProductos()
+    {
+    
+    }
+    public void BuscarProducto()
+    {
+        
+    }
+    public void ActualizarStock()
+    {
+        
+    }
+    public void EliminarProductos()
+    {
+        
+    }
+}
+
+public class Producto
+{
+    public int Id{get;set;}
+    public string Nombre{get;set;}
+    public int Stock{get;set;}
+}
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
