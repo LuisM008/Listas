b4022b6e58227122d54720955a0f4cef60622707
LuisM008 <luiseduardo.monico@upaep.mx>
2026-03-26 07:57:12 -0600

    Create listas.cs

---
+++ listas.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+class Producto
+{
+    public string Nombre{get;set;}
+}
+class Program
+{
+    static void Main()
+    {
+        List<Producto> productos= new List<Producto>();
+        
+        productos.Add(new Producto{Nombre="Mouse"});
+        productos.Add(new Producto{Nombre="Teclado"});
+        productos.Add(new Producto{Nombre="Monitor"});
+        productos.Add(new Producto{Nombre="Audifonos"});
+        productos.Add(new Producto{Nombre="Camara"});
+        productos.Add(new Producto{Nombre="Gabinete"});
+        
+        Console.WriteLine("Lista:");
+        foreach (var p in productos)
+        {
+            Console.WriteLine(p.Nombre);
+        }
+    }
+}
